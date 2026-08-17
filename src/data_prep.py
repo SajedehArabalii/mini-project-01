c5633eca311174d85745ed259d06da3c1d807c1e
Sajedeh Arabali <arabalisajedeh@gamil.com>
2026-08-17 15:16:59 +0330

    feat: first draft

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,3 +1,56 @@
-import pandas as pd
-
-df = pd.read_csv('../data/creditcard.csv')
+""" 
+Preprocessing of data 
+    - loading 
+    - checking 
+    - cleaning 
+    - splitting 
+    - scaling 
+""" 
+import pandas as pd 
+from pathlib import Path 
+from sklearn.model_selection import train_test_split 
+from sklearn.preprocessing import StandardScaler 
+ 
+def prepare_data(): 
+    ROOT = Path(__file__).resolve().parent.parent 
+    DATA_PATH = ROOT / "data" / "creditcard.csv" 
+    df = pd.read_csv(DATA_PATH) 
+ 
+    # # Check data type 
+    # print(f"data types: {df.dtypes}") 
+    # # Number of samples and features 
+    # print(f"samples x features : {df.shape}") 
+    # # Class distribution 
+    # print(f"class distribution : \n{df['Class'].value_counts()}") 
+    # # Number of missing values 
+    # print(f"missing values : \n{df.isna().sum()}") 
+    # # Check for duplicates 
+    # print(f"duplicate : {df.duplicated().sum()}") 
+    # # Descriptive Statistics 
+    # print(f"Discriptive Statistics : {df.describe()}") 
+ 
+    # Remove duplicates 
+    df = df.drop_duplicates() 
+ 
+    # Split your features and target 
+    X = df.drop("Class", axis=1) 
+    y = df["Class"] 
+ 
+    X_train, X_test, y_train, y_test = train_test_split( 
+        X, y, test_size=0.2, stratify=y, random_state=2 
+    ) 
+ 
+    # feature scaling 
+    scaler = StandardScaler() 
+    scaler.fit(X_train) 
+    X_train_scaled = scaler.transform(X_train) 
+    X_test_scaled = scaler.transform(X_test) 
+ 
+    return ( 
+        X_train, 
+        X_test, 
+        y_train, 
+        y_test, 
+ 
+    ) 
+ 
